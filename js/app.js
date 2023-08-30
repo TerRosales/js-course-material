a448ddc8da0a78234e5bb751606e97edcd5aa084
RTIN <118107221+Romo3@users.noreply.github.com>
2023-08-30 16:36:36 -0400

    commiting indeed

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// let js = 'amazing';
+// if (js === 'amazing') alert('Terrence is liking Javascript');
+// console.log(1 + 1 + 2 + 2 + 10);
+
+// let firstName = "Hotaro";
+// let country = 'Erhoda';
+// let continent = 'Area 5';
+// let population = 18392;
+
+
+// console.log(firstName + ' is from ' + country + ' that is located within ' + continent + ' with the highly classified population of ' + population);
+
+let terrencescrpt = true;
+
+console.log(terrencescrpt);
