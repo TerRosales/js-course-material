5bcea543045dc38d072af7a5043250ce628f246b
RTIN <118107221+Romo3@users.noreply.github.com>
2023-09-02 13:55:55 -0400

    lecture 2

--- js/app.js
+++ js/app.js
@@ -2,14 +2,18 @@
 // if (js === 'amazing') alert('Terrence is liking Javascript');
 // console.log(1 + 1 + 2 + 2 + 10);
 
-// let firstName = "Hotaro";
-// let country = 'Erhoda';
-// let continent = 'Area 5';
-// let population = 18392;
+let firstName = "Hotaro";
+let country = 'Erhoda';
+let continent = 'Area 5';
+let population = 18392;
+let isIsland = true;
+let language = "";
+
+console.log (isIsland, population, country, language);
 
 
-// console.log(firstName + ' is from ' + country + ' that is located within ' + continent + ' with the highly classified population of ' + population);
+console.log (typeof isIsland);
+console.log (typeof language);
+console.log (typeof population);
+console.log (typeof firstName);
 
-let terrencescrpt = true;
-
-console.log(terrencescrpt);
