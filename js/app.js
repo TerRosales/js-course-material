02b818681a73410a487b846e90fc95378bae9ef6
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2023-09-02 15:54:09 -0400

    strings, and operators are awesome

--- js/app.js
+++ js/app.js
@@ -7,13 +7,42 @@ let country = 'Erhoda';
 let continent = 'Area 5';
 let population = 18392;
 let isIsland = true;
-let language = "";
+let language = "English";
+const inheritance = "Creation"
+const currentYear = "2056"
+const ageHotaro = currentYear - 2050;
+const ageMiyuka = currentYear - 301;
+let populationFinland = 6000000;
+let populationAverage = 33000000;
 
-console.log (isIsland, population, country, language);
+console.log (ageHotaro, ageMiyuka)
+
+population = population / 2;
+
+console.log (population)
+
+population = population + 1;
+
+console.log (population)
+
+// console.log (population > populationFinland);
+
+// console.log (population > populationAverage);
+
+// console.log (populationFinland - population);
+
+// console.log (populationAverage - population);
+
+// console.log (isIsland, population, country, language);
 
 
-console.log (typeof isIsland);
-console.log (typeof language);
-console.log (typeof population);
-console.log (typeof firstName);
+// console.log (typeof isIsland);
+// console.log (typeof language);
+// console.log (typeof population);
+// console.log (typeof firstName);
 
+console.log (firstName + ' is from ' + country + ' and he inherits the power of creation ')
+
+const hotaroNew = `I am ${firstName} from the land of ${country} and my inheritance is from the God of ${inheritance}, it is please to meet you!`;
+
+console.log (hotaroNew)
