b16e72e80e4009756b0403b0611f3358d94e00e6
Terrence Rosales <118107221+Romo3@users.noreply.github.com>
2023-09-03 14:09:59 -0400

    sunday session

--- js/app.js
+++ js/app.js
@@ -55,11 +55,17 @@
 //     century = 21;
 // }
 // console.log (century);
+// if(population > 33000000) {
+//     console.log ("Erhoda's population is above average")
+// } else {
+//     console.log ("Erhoda's population is below average")
+// }
+
 
 let firstName = "Hotaro";
 let country = 'Erhoda';
 let continent = 'Area 5';
-let population = 18392;
+let population = 8392;
 let isIsland = true;
 let language = "English";
 const inheritance = "Creation"
@@ -67,14 +73,19 @@ const currentYear = "2058"
 const ageHotaro = currentYear - 2050;
 const ageMiyuka = currentYear - 301;
 const hotaroBirthYear = currentYear - ageHotaro;
-let populationFinland = 6000000;
-let populationAverage = 33000000;
+let populationDisqualified = 50000;
+let populationHigh = 500000;
+let populationAverage = 300000;
+let populationLow = 90000;
 const legalAge = 10;
 
-console.log (`${firstName} is from ${country} and he inherits the power of ${inheritance}.`) 
-
-if(population > 33000000) {
-    console.log ("Erhoda's population is above average")
+// console.log (`${firstName} is from ${country} and he inherits the power of ${inheritance}.`) 
+if (population >= populationHigh) {
+    console.log (`You originally belong to a strong well supplied country, please follow protocol and register to a less populated country, please connect with a Relocation agent for more assistance.`)
+} else if(population >= populationAverage) {
+    console.log (`You may now enter, please register your role to this new eutopia`)
+} else if (population >= populationLow) {
+    console.log (`You currently belong to a to a short supplied country, please try again when balances have been made, Take advantage of this flaw by engaging with the country development program. Thank you.`)
 } else {
-    console.log ("Erhoda's population is below average")
+    console.log (`Your country is currently under the disqualification bar, therefore it is protocol procedure for country with these countries to be disbanded and absorbed by country re-selection, please comply and see a Relocation Agent for more information`)
 }
